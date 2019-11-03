d0173297af9a391a9a438e4faa3d27847bbb5ba4
rafayet <rafayet@hashkloud.com>
2019-11-04 01:02:02 +0600

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'welcome#index'
   draw :admin # admin routes 'config/routes/admin.rb'
   draw :api_v1 # api v1 routes 'config/routes/api_v1.rb'
   draw :web # api v1 routes 'config/routes/web.rb'
