dc1ee7829f59f40bed88f45035709d2001bce2ca
rafayet <rafayet@hashkloud.com>
2019-11-07 12:04:40 +0600

    Added templates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,31 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
+
+import '../plugins/popper'
+import '../plugins/bootstrap.min'
+import '../plugins/TweenMax.min'
+import '../plugins/easing'
+import '../plugins/parallax.min'
+import '../plugins/custom'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
+document.addEventListener("turbolinks:click", () => {
+    $(".loader").show();
+});
+
+document.addEventListener("turbolinks:load", () => {
+    $(".loader").hide();
+});
+
+document.addEventListener("ready page:load", () => {
+    $(".loader").hide();
+});
---
+++ app/javascript/plugins/index.js
@@ -0,0 +1,16 @@
+// Load all the channels within this directory and all subdirectories.
+// Channel files must be named *_channel.js.
+
+// const plugins = require.context('.', true)
+// plugins.keys().forEach(plugins)
+
+// require('./popper')
+// require('./bootstrap.min')
+require('./TweenMax.min')
+require('./TimelineMax.min')
+require('./ScrollMagic.min')
+require('./animation.gsap.min')
+require('./ScrollToPlugin.min')
+require('./easing')
+require('./parallax.min')
+require('./custom')
