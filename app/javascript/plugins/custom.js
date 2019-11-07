3a80a208ac8b598a055370957c4fe17c87de4b89
rafayet <rafayet@hashkloud.com>
2019-11-07 17:00:23 +0600

    Partials rendered

--- app/javascript/plugins/custom.js
+++ app/javascript/plugins/custom.js
@@ -28,7 +28,7 @@ $(document).ready(function()
 	var menuActive = false;
 	var menu = $('.menu');
 	var burger = $('.hamburger');
-	var ctrl = new ScrollMagic.Controller();
+	// var ctrl = new ScrollMagic.Controller();
 
 	setHeader();
 
@@ -43,9 +43,9 @@ $(document).ready(function()
 	});
 
 	initMenu();
-	initHeaderSearch();
+	// initHeaderSearch();
 	// initHomeSlider();
-	initMilestones();
+	// initMilestones();
 
 	/* 
 
@@ -123,19 +123,19 @@ $(document).ready(function()
 
 	*/
 
-	function initHeaderSearch()
-	{
-		if($('.search_button').length)
-		{
-			$('.search_button').on('click', function()
-			{
-				if($('.header_search_container').length)
-				{
-					$('.header_search_container').toggleClass('active');
-				}
-			});
-		}
-	}
+	// function initHeaderSearch()
+	// {
+	// 	if($('.search_button').length)
+	// 	{
+	// 		$('.search_button').on('click', function()
+	// 		{
+	// 			if($('.header_search_container').length)
+	// 			{
+	// 				$('.header_search_container').toggleClass('active');
+	// 			}
+	// 		});
+	// 	}
+	// }
 
 	// /*
 	//
@@ -184,55 +184,55 @@ $(document).ready(function()
 
 	*/
 
-	function initMilestones()
-	{
-		if($('.milestone_counter').length)
-		{
-			var milestoneItems = $('.milestone_counter');
-
-	    	milestoneItems.each(function(i)
-	    	{
-	    		var ele = $(this);
-	    		var endValue = ele.data('end-value');
-	    		var eleValue = ele.text();
-
-	    		/* Use data-sign-before and data-sign-after to add signs
-	    		infront or behind the counter number */
-	    		var signBefore = "";
-	    		var signAfter = "";
-
-	    		if(ele.attr('data-sign-before'))
-	    		{
-	    			signBefore = ele.attr('data-sign-before');
-	    		}
-
-	    		if(ele.attr('data-sign-after'))
-	    		{
-	    			signAfter = ele.attr('data-sign-after');
-	    		}
-
-	    		var milestoneScene = new ScrollMagic.Scene({
-		    		triggerElement: this,
-		    		triggerHook: 'onEnter',
-		    		reverse:false
-		    	})
-		    	.on('start', function()
-		    	{
-		    		var counter = {value:eleValue};
-		    		var counterTween = TweenMax.to(counter, 4,
-		    		{
-		    			value: endValue,
-		    			roundProps:"value", 
-						ease: Circ.easeOut, 
-						onUpdate:function()
-						{
-							document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
-						}
-		    		});
-		    	})
-			    .addTo(ctrl);
-	    	});
-		}
-	}
+	// function initMilestones()
+	// {
+	// 	if($('.milestone_counter').length)
+	// 	{
+	// 		var milestoneItems = $('.milestone_counter');
+	//
+	//     	milestoneItems.each(function(i)
+	//     	{
+	//     		var ele = $(this);
+	//     		var endValue = ele.data('end-value');
+	//     		var eleValue = ele.text();
+	//
+	//     		/* Use data-sign-before and data-sign-after to add signs
+	//     		infront or behind the counter number */
+	//     		var signBefore = "";
+	//     		var signAfter = "";
+	//
+	//     		if(ele.attr('data-sign-before'))
+	//     		{
+	//     			signBefore = ele.attr('data-sign-before');
+	//     		}
+	//
+	//     		if(ele.attr('data-sign-after'))
+	//     		{
+	//     			signAfter = ele.attr('data-sign-after');
+	//     		}
+	//
+	//     		var milestoneScene = new ScrollMagic.Scene({
+	// 	    		triggerElement: this,
+	// 	    		triggerHook: 'onEnter',
+	// 	    		reverse:false
+	// 	    	})
+	// 	    	.on('start', function()
+	// 	    	{
+	// 	    		var counter = {value:eleValue};
+	// 	    		var counterTween = TweenMax.to(counter, 4,
+	// 	    		{
+	// 	    			value: endValue,
+	// 	    			roundProps:"value",
+	// 					ease: Circ.easeOut,
+	// 					onUpdate:function()
+	// 					{
+	// 						document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+	// 					}
+	// 	    		});
+	// 	    	})
+	// 		    .addTo(ctrl);
+	//     	});
+	// 	}
+	// }
 
 });
